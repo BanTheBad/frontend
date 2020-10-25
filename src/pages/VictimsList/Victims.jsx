1361cb8eca907282f1b8f1aa5371a41c2b933105
John Otu <johnne01@gmail.com>
2020-10-25 11:42:20 +0100

    chore: Victims and accused list
    Implemented screens to show list of victims and accused officers.
    
    Added AccusedCard unit component.
    Also modified some unit components including:
    * Button to accept _width_ prop to set button width
    * TopNav to accept _withLinks_ prop to determine if top navigation
    should be rendered with or without links.
    
    Fixes #13

---
+++ src/pages/VictimsList/Victims.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { VictimCard } from '../../unit-components';
+
+function Victims() {
+  return (
+    <div>
+      <div style={{ paddingRight: '14px', paddingLeft: '14px', marginTop: '14px' }}>
+        <VictimCard />
+      </div>
+      <div style={{ paddingRight: '14px', paddingLeft: '14px', marginTop: '14px' }}>
+        <VictimCard />
+      </div>
+      <div style={{ paddingRight: '14px', paddingLeft: '14px', marginTop: '14px' }}>
+        <VictimCard />
+      </div>
+    </div>
+  )
+}
+
+export default Victims;
